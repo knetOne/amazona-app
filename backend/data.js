1c160d96028c1fecbf2e9f40ea77910ebe047352
knetOne <k.net@netcourrier.com>
2021-09-13 22:01:15 -0400

    lesson 10

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 10,
+            desciption: 'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Adidas Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 142,
+            countInStock: 20,
+            brand: 'Addidas',
+            rating: 4.5,
+            numReviews: 15,
+            desciption: 'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Lacoste free Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 120,
+            countInStock: 0,
+            brand: 'Lacoste',
+            rating: 4.8,
+            numReviews: 17,
+            desciption: 'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 78,
+            countInStock: 50,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            desciption: 'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Puma Slim Pant',
+            category: 'Pants',
+            image: '/images/p5.jpg',
+            price: 65,
+            countInStock: 0,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            desciption: 'high quality product'
+        },
+        {
+            _id:'6',
+            name:'Adidas Fit Pant',
+            category: 'Shirts',
+            image: '/images/p6.jpg',
+            price: 139,
+            countInStock: 12,
+            brand: 'Addidas',
+            rating: 4.5,
+            numReviews: 15,
+            desciption: 'high quality product'
+        }
+    ]
+}
+
+export default data;
