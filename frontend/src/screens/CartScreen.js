486b5f7def929c62a9fc4dd1b6c3d87d97135d06
knetOne <k.net@netcourrier.com>
2021-09-26 21:41:11 -0400

    Lesson 13-14-15

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../actions/cartAction';
+
+export default function CartScreen(props) {
+    const productId = props.match.params.id;
+    const qty = props.location.search ? Number(props.location.search.split('=')[1]):1;
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (productId){
+            dispatch(addToCart(productId, qty));
+        }
+    }, [dispatch, productId, qty])
+    return (
+        <div>
+            <h1>Car screen</h1>
+            <p>
+                Add to Cart : ProductID: {productId} Qty: {qty}
+            </p>
+        </div>
+    )
+}
