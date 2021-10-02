674d1ef8241da3985b9fb6ec17f44d3e8e8d4592
knetOne <k.net@netcourrier.com>
2021-10-01 20:34:32 -0400

    lesson 19

---
+++ backend/routers/productRouter.js
@@ -0,0 +1,27 @@
+import express from "express";
+import expressAsyncHandler from "express-async-handler";
+import data from "../data.js";
+import Product from "../models/productModel.js";
+
+const productRouter = express.Router();
+
+productRouter.get('/', expressAsyncHandler(async( req, res) =>{
+    const produtcs = await Product.find({});
+    res.send(produtcs);
+}));
+
+productRouter.get('/seed', expressAsyncHandler(async(req, res) =>{
+    //await Product.deleteMany({});
+    const createdProducts = await Product.insertMany(data.products);
+    res.send({createdProducts});
+}));
+
+productRouter.get('/:id', expressAsyncHandler(async(req,res) =>{
+    const product = await Product.findById(req.params.id);
+    if(product){
+        res.send(product);
+    }else{
+        res.status(404).send({ message: 'Product not found'});
+    }
+}))
+export default productRouter;
