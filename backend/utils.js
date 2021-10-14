12ce813b008ac7bd794ad86f90882e173c97eee7
knetOne <k.net@netcourrier.com>
2021-10-14 19:31:11 -0400

    lesson 27

--- backend/utils.js
+++ backend/utils.js
@@ -8,3 +8,20 @@ export const generateToken = (user) => {
         isAdmin: user.isAdmin,
     }, process.env.JWT_SECRET || 'somethingsecret', {expiresIn: '30d'});
 }
+
+export const isAuth = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(authorization){
+        const token = authorization.slice(7, authorization.length); // Bearer XXXXX
+        jwt.verify(token, process.env.JWT_SECRET || 'somethingsecret', (err, decode) =>{
+            if(err) {
+                req.status(401).send({ message: 'Invalid Token'});
+            }else{
+                req.user = decode;
+                next();
+            }
+        })
+    }else{
+        req.status(401).send({ message: 'No Token'});
+    }
+}
