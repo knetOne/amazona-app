fc1ed6045cb22c3a23348e6bf6bf56151b5c0eb8
knetOne <k.net@netcourrier.com>
2021-10-17 16:31:01 -0400

    lesson 31

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -20,6 +20,7 @@ const orderSchema = new mongoose.Schema({
         country: {type: String, required: true},
     },
     paymentMethod: {type: String, required: true},
+    pymentresult: {id: String, status: String, update_time: String, email_address: String},
     itemsPrice: {type: Number, required: true},
     shippingPrice: {type: Number, required: true},
     taxPrice: {type: Number, required: true},
