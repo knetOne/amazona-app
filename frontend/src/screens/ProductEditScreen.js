216f85bc7f7db62c56c7569259e46d8b2e17314f
knetOne <k.net@netcourrier.com>
2021-11-11 19:45:32 -0400

    Lesson 39

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { detailsProduct } from '../actions/productActions';
+import { detailsProduct, updateProduct } from '../actions/productActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstants';
 
 function ProductEditScreen(props) {
     const productId = props.match.params.id;
@@ -14,12 +15,21 @@ function ProductEditScreen(props) {
     const [brand, setBrand] = useState('');
     const [description, setDescription] = useState('');
 
-    const productDetails = useSelector(state => state.productDetails);
+    const productDetails = useSelector((state) => state.productDetails);
     const { loading, error, product }= productDetails;
+
+    const productUpdate = useSelector((state) => state.productUpdate)
+    const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = productUpdate;
+
     const dispatch = useDispatch();
     useEffect(() => {
-        if(!product || product._id !== productId){
+        if(successUpdate){
+            dispatch({type: PRODUCT_UPDATE_RESET});
+            props.history.push('/productlist');
+        }
+        if(!product || product._id !== productId || successUpdate){
             dispatch(detailsProduct(productId));
+            dispatch({type: PRODUCT_UPDATE_RESET});
         }else{
             setName(product.name);            
             setPrice(product.price);          
@@ -33,11 +43,14 @@ function ProductEditScreen(props) {
     },[
         product,
         dispatch,
-        productId
+        productId,
+        successUpdate,
+        props.history
     ]);
     const submitHandler = (e) => {
         e.preventDefault();
         // TODO: dispatch update product
+        dispatch(updateProduct({_id: productId, name, price, image, category, brand, countInStock, description}))
     };
     return (
         <div>
@@ -45,6 +58,8 @@ function ProductEditScreen(props) {
                 <div>
                     <h1>Edit Product {productId}</h1>
                 </div>
+                { loadingUpdate && <LoadingBox></LoadingBox> }
+                {errorUpdate && <MessageBox variant='danger'>{errorUpdate}</MessageBox>}
                 {loading ? <LoadingBox></LoadingBox>
                 : error ? <MessageBox variant="danger">{error}</MessageBox>
                 :
