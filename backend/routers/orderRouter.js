d0fe00ff75f33574642d39984ec569927e8a2a20
knetOne <k.net@netcourrier.com>
2021-11-20 18:55:12 -0400

    lesson 43

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -60,4 +60,14 @@ orderRouter.put('/:id/pay', isAuth, expressAsyncHandler(async (req, res) =>{
     }
 }))
 
+orderRouter.delete('/:id', isAuth, isAdmin, expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if(order){
+        const deleteOrder = await order.remove();
+        res.send({ message: 'Order deleted', order: deleteOrder });
+    } else {
+        res.status(404).send({ message: 'Order not found'})
+    }
+}))
+
 export default orderRouter;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
-import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderMineListReducer, orderListReducer } from './reducers/orderReducers';
+import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderMineListReducer, orderListReducer, orderDeleteReducer } from './reducers/orderReducers';
 import { productCreateReducer, productDeleteReducer, productDetailsReducer, productListReducer, productUpdateReducer } from './reducers/productReducers';
 import { userDetailsReducer, userRegisterReducer, userSigninReducer, userUpdateProfileReducer } from './reducers/userReducers';
 
@@ -36,7 +36,8 @@ const reducer = combineReducers({
     productCreate: productCreateReducer,
     productUpdate: productUpdateReducer,
     productDelete: productDeleteReducer,
-    orderList: orderListReducer
+    orderList: orderListReducer,
+    orderDelete: orderDeleteReducer
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
