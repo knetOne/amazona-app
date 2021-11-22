6fbafb0726d3c34633c1d30e185810c766ab4cf3
knetOne <k.net@netcourrier.com>
2021-11-22 00:28:23 -0400

    correction cdn

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,12 +1,14 @@
+import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa'
+
 export default function Rating (props) {
     const { rating, numReviews } = props;
     return (
         <div className="rating">
-            <span><i className={ rating >=1 ? "fa fa-star" : rating >=0.5 ? "fa fa-star-half-o" : "fa fa-star-0"}></i></span>
-            <span><i className={ rating >=2 ? "fa fa-star" : rating >=1.5 ? "fa fa-star-half-o" : "fa fa-star-0"}></i></span>
-            <span><i className={ rating >=3 ? "fa fa-star" : rating >=2.5 ? "fa fa-star-half-o" : "fa fa-star-0"}></i></span>
-            <span><i className={ rating >=4 ? "fa fa-star" : rating >=3.5 ? "fa fa-star-half-o" : "fa fa-star-0"}></i></span>
-            <span><i className={ rating >=5 ? "fa fa-star" : rating >=4.5 ? "fa fa-star-half-o" : "fa fa-star-0"}></i></span>
+            <span>{ rating >=1 ? <FaStar /> : rating >=0.5 ? <FaStarHalfAlt /> : <FaRegStar />}</span>
+            <span>{ rating >=2 ? <FaStar /> : rating >=1.5 ? <FaStarHalfAlt /> : <FaRegStar />}</span>
+            <span>{ rating >=3 ? <FaStar /> : rating >=2.5 ? <FaStarHalfAlt /> : <FaRegStar />}</span>
+            <span>{ rating >=4 ? <FaStar /> : rating >=3.5 ? <FaStarHalfAlt /> : <FaRegStar />}</span>
+            <span>{ rating >=5 ? <FaStar /> : rating >=4.5 ? <FaStarHalfAlt /> : <FaRegStar />}</span>
             <span>{numReviews + ' reviews'}</span>
         </div>
     )
